451bd9db7dd1a07f9460ebc036293d44e999dfe4
Aleksandar Dekan <acodekan345@gmail.com>
2026-06-30 10:24:41 +0200

    Fixes added type to export Category.

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,5 +3,4 @@ export { RegisterDto, LoginDto } from './dto/auth.dto';
 export type { AuthResponseDto } from './dto/auth.dto';
 export type { JwtPayload } from './interfaces/jwt-payload.interface';
 export { CreateCategoryDto, UpdateCategoryDto } from './dto/category.dto';
-export {CreateSupplierDto, UpdateSupplierDto} from './dto/supplier.dto';
-export { Category } from './dto/category.dto';
+export type { Category } from './dto/category.dto';
